60a32c208df523c0cc6ab253d6c0e16c580c9f02
luiz.caetano <luiz.caetano@MMQ003477>
2023-09-22 14:45:34 -0300

    Adicionar arquivos de projeto.

---
+++ URI1008/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class URI1008
+    {
+        static void Main(string[] args)
+        {
+
+            double number, HorasTrabalhadas, salary, ValorHora;
+
+            number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            HorasTrabalhadas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ValorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salary = ValorHora * HorasTrabalhadas;
+
+            Console.WriteLine("NUMBER = " + number);
+            Console.WriteLine("SALARY = U$ " + salary.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
